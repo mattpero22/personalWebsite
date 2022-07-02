b0cd211aae6135ee8af34482786ffe5298af4a06
matt pero <mattpero22@gmail.com>
2022-07-02 09:06:37 -0400

    add: main and footer component templates

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 // components / pages
-import './components/Header'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Main from './components/Main';
 // stylesheets
 import './App.css';
-import Header from './components/Header';
+
 
 function App() {
   return (
     <p>
       <Header> </Header>
+      <Footer> </Footer>
+      <Main> </Main>
     </p>
   );
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,7 @@
+const Main = () => {
+    return (
+        <h1>MAIN COMPONENT</h1>
+    );
+}
+
+export default Main;
