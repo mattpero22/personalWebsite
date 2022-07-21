256bcbc1cc613a0778325e47564c1d0469938ba6
matt pero <mattpero22@gmail.com>
2022-07-21 06:32:46 -0400

    fixed flexbox styling on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,17 @@
 const Home = () => {
     return (
         <>
-            <div className="aware-area">
-                <div className="home-name" id="home-first">
-                    <div id="M">M</div>
-                    <div id="A">A</div>
-                    <div id="T1">T</div>
-                    <div id="T2">T</div>
-                </div>
-                <div className="home-name" id="home-last">
-                    <div id="P">P</div>
-                    <div id="E">E</div>
-                    <div id="R">R</div>
-                    <div id="O">O</div>
-                </div>
+            <div className="home-name" id="home-first">
+                <div className="letter" id="M">M</div>
+                <div className="letter" id="A">A</div>
+                <div className="letter" id="T1">T</div>
+                <div className="letter" id="T2">T</div>
+            </div>
+            <div className="home-name" id="home-last">
+                <div className="letter" id="P">P</div>
+                <div className="letter" id="E">E</div>
+                <div className="letter" id="R">R</div>
+                <div className="letter" id="O">O</div>
             </div>
         </>
     )
