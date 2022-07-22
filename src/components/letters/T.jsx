508d27f9055e94431173bd98badc64de5acc7f77
matt pero <mattpero22@gmail.com>
2022-07-22 06:19:17 -0400

    added the cross to the trunk with left and right branches

--- src/components/letters/T.jsx
+++ src/components/letters/T.jsx
@@ -3,6 +3,8 @@ const T = () => {
     return (
         <div className="letter" id="T">
             <div className="trunk" id="base"></div>
+            <div className="branch" id="left_branch"></div>
+            <div className="branch" id="right_branch"></div>
         </div>
     )
 }
