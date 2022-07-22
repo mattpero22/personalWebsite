bdd296f0de390d94db3738997135430d74df3fe9
matt pero <mattpero22@gmail.com>
2022-07-22 07:57:24 -0400

    built out first prototype of tree

--- src/components/letters/T.jsx
+++ src/components/letters/T.jsx
@@ -5,6 +5,9 @@ const T = () => {
             <div className="trunk" id="base"></div>
             <div className="branch" id="left_branch"></div>
             <div className="branch" id="right_branch"></div>
+            <div className="leaf" id="bot_leaf"></div>
+            <div className="leaf" id="mid_leaf"></div>
+            <div className="leaf" id="top_leaf"></div>
         </div>
     )
 }
