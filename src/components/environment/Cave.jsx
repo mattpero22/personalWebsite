ca91fbbe6bb2ff1af213f934572b4dbf4a3268ab
matt pero <mattpero22@gmail.com>
2022-07-25 19:56:36 -0400

    reworked cave

--- src/components/environment/Cave.jsx
+++ src/components/environment/Cave.jsx
@@ -1,20 +1,15 @@
 const Cave = () => {
     return (
-        <div id="cave">
-            <div id="layer3">
-                <div id="layer2">
-                    <div id="layer1">
-                        <div className="home-name" id="home-last">
-                            <div className="cave_letter"id="P">P</div>
-                            <div className="cave_letter"id="E">E</div>
-                            <div className="cave_letter"id="R">R</div>
-                            <div className="cave_letter"id="O">O</div>
-                        </div>
-                        <img class="emerald" src="https://i.imgur.com/UkP0K3g.png"></img>
-                        <img class="amethyst" src="https://i.imgur.com/WpWNKGL.png"></img>
-                    </div>
-                </div>
+        <div class="cave">
+            <div id="roof">
+                <div className="stone1"></div>
+                <div className="stone2"></div>
             </div>
+            <div id="floor">
+                <div className="stone2"></div>
+                <div className="stone1"></div>
+            </div>
+
         </div>
     )
 }
