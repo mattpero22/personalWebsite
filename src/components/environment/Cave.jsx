e1ed163614d79280becbd13571b1bff1ab1ad4ab
matt pero <mattpero22@gmail.com>
2022-07-25 16:47:25 -0400

    lots of ideas and styling added to the underground

--- src/components/environment/Cave.jsx
+++ src/components/environment/Cave.jsx
@@ -5,11 +5,13 @@ const Cave = () => {
                 <div id="layer2">
                     <div id="layer1">
                         <div className="home-name" id="home-last">
-                            <div className="letter" id="P">P</div>
-                            <div className="letter" id="E">E</div>
-                            <div className="letter" id="R">R</div>
-                            <div className="letter" id="O">O</div>
+                            <div className="cave_letter"id="P">P</div>
+                            <div className="cave_letter"id="E">E</div>
+                            <div className="cave_letter"id="R">R</div>
+                            <div className="cave_letter"id="O">O</div>
                         </div>
+                        <img class="emerald" src="https://i.imgur.com/UkP0K3g.png"></img>
+                        <img class="amethyst" src="https://i.imgur.com/WpWNKGL.png"></img>
                     </div>
                 </div>
             </div>
---
+++ src/pages/Attributions.jsx
@@ -0,0 +1,4 @@
+<div>
+    <p>Black and white gemstone images used in underground detail</p>
+    <a href='https://www.freepik.com/vectors/quartz'>Quartz vector created by macrovector - www.freepik.com</a>
+</div>    
