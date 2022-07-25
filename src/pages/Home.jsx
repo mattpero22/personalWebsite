3684135c1645d24e3e4b711cd2564a85c6802262
matt pero <mattpero22@gmail.com>
2022-07-25 06:36:29 -0400

    working on the ground, and then underground styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,6 +11,10 @@ const Home = () => {
                 <T> </T>
                 <T> </T>
             </div>
+            <div className="ground">
+                <div className="ground" id="grass1"></div>
+                <div className="ground" id="grass2"></div>
+            </div>
             <div className="home-name" id="home-last">
                 <div className="letter" id="P">P</div>
                 <div className="letter" id="E">E</div>
