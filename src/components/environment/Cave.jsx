68dcb9bd8c2892afb3c6e52157f98aba71790a34
matt pero <mattpero22@gmail.com>
2022-07-26 08:05:22 -0400

    added styles for the tites on the top left

--- src/components/environment/Cave.jsx
+++ src/components/environment/Cave.jsx
@@ -1,22 +1,61 @@
 const Cave = () => {
     return (
         <div class="cave">
-            <div id="roof">
+            <div id="roof_left">
                 <div className="stone1">
-                    <div className="tite1 small" id='t1-1'></div>
-                    <div className="tite1 med" id='t1-2'></div>
-                    <div className="tite1 large" id='t1-3'></div>
-                    <div className="tite1 xl" id='t1-4'></div>
-                    <div className="tite1 med" id='t1-5'></div>
-                    <div className="tite1 med" id='t1-6'></div>
+                    <div className="tite1 small"></div>
+                    <div className="tite1 med"></div>
+                    <div className="tite1 large"></div>
+                    <div className="tite1 xl"></div>
+                    <div className="tite1 med"></div>
+                    <div className="tite1 med"></div>
+                    <div className="tite1 small"></div>
+                    <div className="tite1 med"></div>
+                    <div className="tite1 large"></div>
+                    <div className="tite1 xl"></div>
+                    <div className="tite1 large"></div>
+                    <div className="tite1 med"></div>
+                    <div className="tite1 med"></div>
+                    <div className="tite1 small"></div>
+                    <div className="tite1 small"></div>
+                    <div className="tite1 med"></div>
+                    <div className="tite1 small"></div>
+                    <div className="tite1 large"></div>
+                    <div className="tite1 med"></div>
+                    <div className="tite1 small"></div>
+                    <div className="tite1 med"></div>
                 </div>
                 <div className="stone2">
-                    <div className="tite2" id="t2-1"></div>
+                    <div className="tite2 large2"></div>
+                    <div className="tite2 small2"></div>
+                    <div className="tite2 xl2"></div>
+                    <div className="tite2 small2"></div>
+                    <div className="tite2 med2"></div>
+                    <div className="tite2 large2"></div>
+                    <div className="tite2 large2"></div>
+                    <div className="tite2 med2"></div>
+                    <div className="tite2 small2"></div>
+                    <div className="tite2 small2"></div>
+                    <div className="tite2 xl2"></div>
+                    <div className="tite2 large2"></div>
+                    <div className="tite2 large2"></div>
+                    <div className="tite2 med2"></div>
+                    <div className="tite2 small2"></div>
+                    <div className="tite2 med2"></div>
+                    <div className="tite2 med2"></div>
+                    <div className="tite2 small2"></div>
+                    <div className="tite2 small2"></div>
                 </div>
             </div>
-            <div id="floor">
-                <div className="stone2"></div>
-                <div className="stone1"></div>
+
+
+            <div id="floor_left">
+                <div className="stone2">
+                    <div className="mite1"></div>
+                </div>
+                <div className="stone1">
+
+                </div>
             </div>
 
         </div>
