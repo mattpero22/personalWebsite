41fba4d5a328300dfdcd23e55ceaad93b4391d10
matt pero <mattpero22@gmail.com>
2022-07-26 06:33:55 -0400

    defined classes for 4 different sizes of tites/mites

--- src/components/environment/Cave.jsx
+++ src/components/environment/Cave.jsx
@@ -3,7 +3,12 @@ const Cave = () => {
         <div class="cave">
             <div id="roof">
                 <div className="stone1">
-                    <div className="tite1" id='t1-1'></div>
+                    <div className="tite1 small" id='t1-1'></div>
+                    <div className="tite1 med" id='t1-2'></div>
+                    <div className="tite1 large" id='t1-3'></div>
+                    <div className="tite1 xl" id='t1-4'></div>
+                    <div className="tite1 med" id='t1-5'></div>
+                    <div className="tite1 med" id='t1-6'></div>
                 </div>
                 <div className="stone2">
                     <div className="tite2" id="t2-1"></div>
