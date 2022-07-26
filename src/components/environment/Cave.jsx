9ceea8044cf795d7f4749fb87cf95e5e6d408b99
matt pero <mattpero22@gmail.com>
2022-07-26 10:11:28 -0400

    added css and html for the top of the P

--- src/components/environment/Cave.jsx
+++ src/components/environment/Cave.jsx
@@ -1,8 +1,9 @@
 const Cave = () => {
     return (
         <div className="cave">
-            <div id="roof_left">
+            <div id="roof">
                 <div className="stone1">
+                    {/* left misc front*/}
                     <div className="tite small"></div>
                     <div className="tite med"></div>
                     <div className="tite large"></div>
@@ -24,8 +25,23 @@ const Cave = () => {
                     <div className="tite med"></div>
                     <div className="tite small"></div>
                     <div className="tite med"></div>
+                    {/* P front */}
+                    {/* LETTER */}
+                    <div className="tite" id="p11"></div>
+                    <div className="tite" id="p12"></div>
+                    <div className="tite" id="p13"></div>
+                    <div className="tite" id="p12"></div>
+                    <div className="tite" id="p13"></div>
+                    <div className="tite" id="p13"></div>
+                    <div className="tite" id="p12"></div>
+                    <div className="tite" id="p13"></div>
+                    <div className="tite" id="p12"></div>
+                    <div className="tite" id="p14"></div>
+                    <div className="tite" id="p15"></div>
+                    {/* SPACING */}
                 </div>
                 <div className="stone2">
+                    {/* left misc back */}
                     <div className="tite large2"></div>
                     <div className="tite small2"></div>
                     <div className="tite xl2"></div>
@@ -45,8 +61,26 @@ const Cave = () => {
                     <div className="tite med2"></div>
                     <div className="tite small2"></div>
                     <div className="tite small2"></div>
+                    {/* P back */}
+                    <div className="tite" id="p21"></div>
+                    <div className="tite" id="p22"></div>
+                    <div className="tite" id="p23"></div>
+                    <div className="tite" id="p23"></div>
+                    <div className="tite" id="p23"></div>
+                    <div className="tite" id="p23"></div>
+                    <div className="tite" id="p23"></div>
+                    <div className="tite" id="p23"></div>
+                    <div className="tite" id="p22"></div>
+                    <div className="tite" id="p24"></div>
+                    <div className="tite" id="p25"></div>
                 </div>
             </div>
+
+            <div>
+
+            </div>
+
+
 
 
             <div id="floor_left">
