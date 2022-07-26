0d25809f174db0d6a842a07495ee8f2305bfc83e
matt pero <mattpero22@gmail.com>
2022-07-26 06:20:55 -0400

    added the roof layers and a stalactite in each

--- src/components/environment/Cave.jsx
+++ src/components/environment/Cave.jsx
@@ -2,8 +2,12 @@ const Cave = () => {
     return (
         <div class="cave">
             <div id="roof">
-                <div className="stone1"></div>
-                <div className="stone2"></div>
+                <div className="stone1">
+                    <div className="tite1" id='t1-1'></div>
+                </div>
+                <div className="stone2">
+                    <div className="tite2" id="t2-1"></div>
+                </div>
             </div>
             <div id="floor">
                 <div className="stone2"></div>
