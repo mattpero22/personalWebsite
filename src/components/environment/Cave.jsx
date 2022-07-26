a3ecab53d75723b99db2270f14dd8ac73a7e89f3
matt pero <mattpero22@gmail.com>
2022-07-26 13:50:24 -0400

    added stone font and name to cave

--- src/components/environment/Cave.jsx
+++ src/components/environment/Cave.jsx
@@ -3,7 +3,6 @@ const Cave = () => {
         <div className="cave">
             <div id="roof">
                 <div className="stone1">
-                    {/* left misc front*/}
                     <div className="tite small1"></div>
                     <div className="tite med1"></div>
                     <div className="tite large1"></div>
@@ -25,23 +24,92 @@ const Cave = () => {
                     <div className="tite med1"></div>
                     <div className="tite small1"></div>
                     <div className="tite med1"></div>
-                    {/* P front */}
-                    {/* LETTER */}
-                    <div className="tite" id="p11"></div>
-                    <div className="tite" id="p12"></div>
-                    <div className="tite" id="p13"></div>
-                    <div className="tite" id="p12"></div>
-                    <div className="tite" id="p13"></div>
-                    <div className="tite" id="p13"></div>
-                    <div className="tite" id="p12"></div>
-                    <div className="tite" id="p13"></div>
-                    <div className="tite" id="p12"></div>
-                    <div className="tite" id="p14"></div>
-                    <div className="tite" id="p15"></div>
-                    {/* SPACING */}
+                    <div className="tite small1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite large1"></div>
+                    <div className="tite xl1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite small1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite large1"></div>
+                    <div className="tite xl1"></div>
+                    <div className="tite large1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite small1"></div>
+                    <div className="tite small1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite small1"></div>
+                    <div className="tite large1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite small1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite small1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite large1"></div>
+                    <div className="tite xl1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite small1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite large1"></div>
+                    <div className="tite xl1"></div>
+                    <div className="tite large1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite small1"></div>
+                    <div className="tite small1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite small1"></div>
+                    <div className="tite large1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite small1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite small1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite large1"></div>
+                    <div className="tite xl1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite small1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite large1"></div>
+                    <div className="tite xl1"></div>
+                    <div className="tite large1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite small1"></div>
+                    <div className="tite small1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite small1"></div>
+                    <div className="tite large1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite small1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite small1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite large1"></div>
+                    <div className="tite xl1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite small1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite large1"></div>
+                    <div className="tite xl1"></div>
+                    <div className="tite large1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite small1"></div>
+                    <div className="tite small1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite small1"></div>
+                    <div className="tite large1"></div>
+                    <div className="tite med1"></div>
+                    <div className="tite small1"></div>
+                    <div className="tite med1"></div>
                 </div>
                 <div className="stone2">
-                    {/* left misc back */}
                     <div className="tite large2"></div>
                     <div className="tite small2"></div>
                     <div className="tite xl2"></div>
@@ -61,19 +129,87 @@ const Cave = () => {
                     <div className="tite med2"></div>
                     <div className="tite small2"></div>
                     <div className="tite small2"></div>
-                    {/* P back */}
-                    <div className="tite" id="p21"></div>
-                    <div className="tite" id="p22"></div>
-                    <div className="tite" id="p23"></div>
-                    <div className="tite" id="p23"></div>
-                    <div className="tite" id="p23"></div>
-                    <div className="tite" id="p23"></div>
-                    <div className="tite" id="p23"></div>
-                    <div className="tite" id="p23"></div>
-                    <div className="tite" id="p22"></div>
-                    <div className="tite" id="p24"></div>
-                    <div className="tite" id="p25"></div>
+                    <div className="tite large2"></div>
+                    <div className="tite small2"></div>
+                    <div className="tite xl2"></div>
+                    <div className="tite small2"></div>
+                    <div className="tite med2"></div>
+                    <div className="tite large2"></div>
+                    <div className="tite large2"></div>
+                    <div className="tite med2"></div>
+                    <div className="tite small2"></div>
+                    <div className="tite small2"></div>
+                    <div className="tite xl2"></div>
+                    <div className="tite large2"></div>
+                    <div className="tite large2"></div>
+                    <div className="tite med2"></div>
+                    <div className="tite small2"></div>
+                    <div className="tite med2"></div>
+                    <div className="tite med2"></div>
+                    <div className="tite small2"></div>
+                    <div className="tite small2"></div>
+                    <div className="tite large2"></div>
+                    <div className="tite small2"></div>
+                    <div className="tite xl2"></div>
+                    <div className="tite small2"></div>
+                    <div className="tite med2"></div>
+                    <div className="tite large2"></div>
+                    <div className="tite large2"></div>
+                    <div className="tite med2"></div>
+                    <div className="tite small2"></div>
+                    <div className="tite small2"></div>
+                    <div className="tite xl2"></div>
+                    <div className="tite large2"></div>
+                    <div className="tite large2"></div>
+                    <div className="tite med2"></div>
+                    <div className="tite small2"></div>
+                    <div className="tite med2"></div>
+                    <div className="tite med2"></div>
+                    <div className="tite small2"></div>
+                    <div className="tite small2"></div>
+                    <div className="tite large2"></div>
+                    <div className="tite small2"></div>
+                    <div className="tite xl2"></div>
+                    <div className="tite small2"></div>
+                    <div className="tite med2"></div>
+                    <div className="tite large2"></div>
+                    <div className="tite large2"></div>
+                    <div className="tite med2"></div>
+                    <div className="tite small2"></div>
+                    <div className="tite small2"></div>
+                    <div className="tite xl2"></div>
+                    <div className="tite large2"></div>
+                    <div className="tite large2"></div>
+                    <div className="tite med2"></div>
+                    <div className="tite small2"></div>
+                    <div className="tite med2"></div>
+                    <div className="tite med2"></div>
+                    <div className="tite small2"></div>
+                    <div className="tite small2"></div>
+                    <div className="tite large2"></div>
+                    <div className="tite small2"></div>
+                    <div className="tite xl2"></div>
+                    <div className="tite small2"></div>
+                    <div className="tite med2"></div>
+                    <div className="tite large2"></div>
+                    <div className="tite large2"></div>
+                    <div className="tite med2"></div>
+                    <div className="tite small2"></div>
+                    <div className="tite small2"></div>
+                    <div className="tite xl2"></div>
+                    <div className="tite large2"></div>
+                    <div className="tite large2"></div>
+                    <div className="tite med2"></div>
+                    <div className="tite small2"></div>
+                    <div className="tite med2"></div>
+                    <div className="tite med2"></div>
+                    <div className="tite small2"></div>
+                    <div className="tite small2"></div>
                 </div>
+            </div>
+
+            <div className="home-name" id="home-last">
+                P E R O 
             </div>
 
             <div id="floor">
