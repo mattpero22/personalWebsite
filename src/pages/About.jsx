6313d5b297104c386dfac13fa309a27640950403
matt pero <mattpero22@gmail.com>
2022-07-28 06:45:50 -0400

    added icons onto my about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,6 +10,21 @@ const About = () => {
                 </div>
                 <div id="bio">
                     <h2>Hi. I'm Matt Pero. </h2>
+                    <h3>Mechanical Engineer turned Software Engineer with interests in:</h3>
+                    <ul>
+                        <div className="interest">
+                            <img src="https://i.imgur.com/MR1WKNF.png"></img>
+                            <h4>Full Stack Development</h4>
+                        </div>
+                        <div className="interest">
+                            <img src="https://i.imgur.com/d79f0Ai.png"></img>
+                            <h4>Data Analysis and Visualization</h4>
+                        </div>
+                        <div className="interest">
+                            <img src="https://i.imgur.com/mZR1Xh7.png"></img>
+                            <h4>Control Systems</h4>
+                        </div>
+                    </ul>
                 </div>
             </div>
             <Sidebar> </Sidebar>
