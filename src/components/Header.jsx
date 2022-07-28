d4c4cf33025c0d5584195756d9b59ee85dd8fb42
matt pero <mattpero22@gmail.com>
2022-07-28 13:42:26 -0400

    made background better

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import { HashLink } from 'react-router-hash-link'
 const Header = () => {
     return (
             <ul className="Header">
-                <HashLink smooth to={'/#'}>Home</HashLink>
-                <HashLink smooth to={'/#about'}>About</HashLink>
+                <HashLink className='header_link hover_animate' smooth to={'/#'}>Home</HashLink>
+                <HashLink className='header_link hover_animate' smooth to={'/#about'}>About</HashLink>
                 <li>Projects</li>
                 <li>Resume</li>
                 <li>Contact</li>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,23 +6,23 @@ const About = () => {
             <Sidebar> </Sidebar>
             <div className='about_content'>
                 <div id="headshot">
-                    <img className="hover_animate" src="https://i.imgur.com/gLhrTYe.jpg"></img>
+                    <img src="https://i.imgur.com/q42XMfv.png" alt="headshot"></img>
                 </div>
                 <div id="bio">
                     <h2>Hi. I'm Matt Pero. </h2>
                     <h3>Mechanical Engineer turned Software Engineer.</h3>
-                    <h3>Interested in:.</h3>
+                    <h3>Interested in:</h3>
                     <ul>
                         <div className="interest">
-                            <img className="hover_animate" src="https://i.imgur.com/MR1WKNF.png"></img>
+                            <img className="hover_animate" src="https://i.imgur.com/MR1WKNF.png" alt="stack icon"></img>
                             <h4>Full Stack Development</h4>
                         </div>
                         <div className="interest">
-                            <img className="hover_animate" src="https://i.imgur.com/d79f0Ai.png"></img>
+                            <img className="hover_animate" src="https://i.imgur.com/d79f0Ai.png" alt="data icon"></img>
                             <h4>Data Analysis and Visualization</h4>
                         </div>
                         <div className="interest">
-                            <img className="hover_animate" src="https://i.imgur.com/mZR1Xh7.png"></img>
+                            <img className="hover_animate" src="https://i.imgur.com/mZR1Xh7.png" alt="robot icon"></img>
                             <h4>Control Systems</h4>
                         </div>
                     </ul>
