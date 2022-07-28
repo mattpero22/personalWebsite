30c8e1b4c902de6eef4045db7aacc81953de0292
matt pero <mattpero22@gmail.com>
2022-07-28 05:36:06 -0400

    commit of yesterday's work. spinning up today

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,9 +6,21 @@ const Sidebar = () => {
                 <div className="tite med1"></div>
                 <div className="tite large1"></div>
                 <div className="tite xl1"></div>
+                <div className="tite small1"></div>
                 <div className="tite med1"></div>
+                <div className="tite large1"></div>
+                <div className="tite med1"></div>
+                <div className="tite small1"></div>
             </div>
-
+            <div className="stone2">
+                <div className="tite large2"></div>
+                <div className="tite small2"></div>
+                <div className="tite xl2"></div>
+                <div className="tite small2"></div>
+                <div className="tite med2"></div>
+                <div className="tite large2"></div>
+                <div className="tite large2"></div>
+            </div>
         </div>
     )
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,9 +4,14 @@ const About = () => {
     return (
         <div className="About" id="about">
             <Sidebar> </Sidebar>
-            <div className='main content'></div>
-                <img src="https://i.imgur.com/gLhrTYe.jpg"></img>
-                <h2>Hi. I'm Matt Pero. </h2>
+            <div className='about_content'>
+                <div id="headshot">
+                    <img src="https://i.imgur.com/gLhrTYe.jpg"></img>
+                </div>
+                <div id="bio">
+                    <h2>Hi. I'm Matt Pero. </h2>
+                </div>
+            </div>
             <Sidebar> </Sidebar>
         </div>
     )
