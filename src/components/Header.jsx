39b8ae0d42015558ec97fae5030abe381210da1e
matt pero <mattpero22@gmail.com>
2022-07-28 15:54:59 -0400

    set up icons for the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,11 @@ import { HashLink } from 'react-router-hash-link'
 const Header = () => {
     return (
             <ul className="Header">
-                <HashLink className='header_link hover_animate' smooth to={'/#'}>Home</HashLink>
-                <HashLink className='header_link hover_animate' smooth to={'/#about'}>About</HashLink>
-                <li>Projects</li>
-                <li>Resume</li>
-                <li>Contact</li>
+                <HashLink className='header_link hover_animate' smooth to={'/#'}><img src="https://i.imgur.com/jYrWq1Z.png" alt="home"></img></HashLink>
+                <HashLink className='header_link hover_animate' smooth to={'/#about'}><img src="https://i.imgur.com/mPcpvxE.png" alt="me"></img></HashLink>
+                <HashLink className='header_link hover_animate' smooth to={'/#about'}><img src="https://i.imgur.com/glFzRCv.png" alt="skills"></img></HashLink>
+                <HashLink className='header_link hover_animate' smooth to={'/#about'}><img src="https://i.imgur.com/aHKbJtv.png" alt="projects"></img></HashLink>
+                <HashLink className='header_link hover_animate' smooth to={'/#about'}><img src="https://i.imgur.com/RFHj9bL.png" alt="contact"></img></HashLink>
             </ul>
     );
 }
