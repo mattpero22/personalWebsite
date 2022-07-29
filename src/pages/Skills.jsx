959a2a7c67094180a60de0751abc6b64c2e12023
matt pero <mattpero22@gmail.com>
2022-07-29 06:48:51 -0400

    added more skill icons

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -5,14 +5,23 @@ const Skills = () => {
                 <h2>My Skills</h2>
                 <h3>Languages</h3>
                     <div className="skill_icons">
-                        <img className="skill_icon" src="https://i.imgur.com/0DeXqd6.png"/>
-                        <img className="skill_icon" src="https://i.imgur.com/gyNinVG.png"/>
-                        <img className="skill_icon" src="https://i.imgur.com/w5Y6YND.png"/>
-                    <img className="skill_icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" />
+                        <img className="skill_icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" alt="python"/>
+                        <img className="skill_icon" src="https://i.imgur.com/0DeXqd6.png" alt="html"/>
+                        <img className="skill_icon" src="https://i.imgur.com/gyNinVG.png" alt="css"/>
+                        <img className="skill_icon" src="https://i.imgur.com/w5Y6YND.png" alt="javascript"/>
+                        <img className="skill_icon" src="https://i.imgur.com/YvPtI2n.png" alt="sql"/>
+                        <img className="skill_icon" src="https://i.imgur.com/TK2fhAg.png" alt="matlab"/>
+                        <img className="skill_icon" src="https://i.imgur.com/rK6cgqb.png" alt="c"/>
+                        <img className="skill_icon" src="https://i.imgur.com/J9vqWty.png" alt="c++"/>
                     </div>
                 <h3>Libraries and Frameworks</h3>
                     <div className="skill_icons">
-
+                        <img className="skill_icon" src="https://i.imgur.com/ePTfjzt.png" alt="react"/>
+                        <img className="skill_icon" src="https://i.imgur.com/Ke6koPJ.png" alt="node.js"/>
+                        <img className="skill_icon" src="https://i.imgur.com/BX4UdjY.png" alt="express.js"/>
+                        <img className="skill_icon" src="https://i.imgur.com/5xk8xhR.png" alt="mongodb"/>
+                        <img className="skill_icon" src="https://i.imgur.com/gQMAhsd.png" alt="jquery"/>
+                        <img className="skill_icon" id="pandas" src="https://pandas.pydata.org/static/img/pandas_secondary.svg" alt="pandas"/>
                     </div>
                 <h3>Software</h3>
                     <div className="skill_icons">
