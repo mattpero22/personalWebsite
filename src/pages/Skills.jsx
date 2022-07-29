cab22f45a546eed0ecbb4e56ad889daba0dbb3b9
matt pero <mattpero22@gmail.com>
2022-07-28 20:09:41 -0400

    added some skill badges and a basic animation

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,7 +1,25 @@
 const Skills = () => {
     return (
         <div className="Skills" id="skills">
-            <h2>Skills</h2>
+            <div className="skills_content">
+                <h2>My Skills</h2>
+                <h3>Languages</h3>
+                    <div className="skill_icons">
+                        <img className="skill_icon" src="https://i.imgur.com/0DeXqd6.png"/>
+                        <img className="skill_icon" src="https://i.imgur.com/gyNinVG.png"/>
+                        <img className="skill_icon" src="https://i.imgur.com/w5Y6YND.png"/>
+                    <img className="skill_icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" />
+                    </div>
+                <h3>Libraries and Frameworks</h3>
+                    <div className="skill_icons">
+
+                    </div>
+                <h3>Software</h3>
+                    <div className="skill_icons">
+                        
+                    </div>
+
+            </div>
         </div>
     )
 }
