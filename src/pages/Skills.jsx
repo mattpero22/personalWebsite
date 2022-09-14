258331a7a363f01d8e5d74a67041e4a44a6ad2cd
matt pero <mattpero22@gmail.com>
2022-09-14 13:09:06 -0400

    added tooltips for languages and libraries and frameworkds

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -21,7 +21,7 @@ const Skills = () => {
                             onMouseEnter={() => showTooltip(true)}
                             onMouseLeave={() => {
                                   showTooltip(false);
-                                  setTimeout(() => showTooltip(true), 50);
+                                  setTimeout(() => showTooltip(true), 0);
                                 }}
                             />
                         </>
@@ -35,33 +35,254 @@ const Skills = () => {
                             onMouseEnter={() => showTooltip(true)}
                             onMouseLeave={() => {
                                   showTooltip(false);
-                                  setTimeout(() => showTooltip(true), 50);
+                                  setTimeout(() => showTooltip(true), 0);
                                 }}
                             />
-                        </>     
-                        <img className="skill_icon" src="https://i.imgur.com/gyNinVG.png" alt="css"/>
-                        <img className="skill_icon" src="https://i.imgur.com/w5Y6YND.png" alt="javascript"/>
-                        <img className="skill_icon" src="https://i.imgur.com/YvPtI2n.png" alt="sql"/>
-                        <img className="skill_icon" src="https://i.imgur.com/TK2fhAg.png" alt="matlab"/>
-                        <img className="skill_icon" src="https://i.imgur.com/rK6cgqb.png" alt="c"/>
-                        <img className="skill_icon" src="https://i.imgur.com/J9vqWty.png" alt="c++"/>
+                        </>
+                        <>
+                        {tooltip && <ReactTooltip effect="solid" />}
+                            <img 
+                            className="skill_icon" 
+                            src="https://i.imgur.com/gyNinVG.png" 
+                            alt="css" 
+                            data-tip="CSS"
+                            onMouseEnter={() => showTooltip(true)}
+                            onMouseLeave={() => {
+                                  showTooltip(false);
+                                  setTimeout(() => showTooltip(true), 0);
+                                }}
+                            />
+                        </>
+                        <>
+                        {tooltip && <ReactTooltip effect="solid" />}
+                            <img 
+                            className="skill_icon" 
+                            src="https://i.imgur.com/w5Y6YND.png"  
+                            alt="js" 
+                            data-tip="JavaScript"
+                            onMouseEnter={() => showTooltip(true)}
+                            onMouseLeave={() => {
+                                  showTooltip(false);
+                                  setTimeout(() => showTooltip(true), 0);
+                                }}
+                            />
+                        </>
+                        <>
+                        {tooltip && <ReactTooltip effect="solid" />}
+                            <img 
+                            className="skill_icon" 
+                            src="https://i.imgur.com/YvPtI2n.png"  
+                            alt="sql" 
+                            data-tip="SQL"
+                            onMouseEnter={() => showTooltip(true)}
+                            onMouseLeave={() => {
+                                  showTooltip(false);
+                                  setTimeout(() => showTooltip(true), 0);
+                                }}
+                            />
+                        </>
+                        <>
+                        {tooltip && <ReactTooltip effect="solid" />}
+                            <img 
+                            className="skill_icon" 
+                            src="https://i.imgur.com/TK2fhAg.png" 
+                            alt="matlab" 
+                            data-tip="MATLAB"
+                            onMouseEnter={() => showTooltip(true)}
+                            onMouseLeave={() => {
+                                  showTooltip(false);
+                                  setTimeout(() => showTooltip(true), 0);
+                                }}
+                            />
+                        </>
+                        <>
+                        {tooltip && <ReactTooltip effect="solid" />}
+                            <img 
+                            className="skill_icon" 
+                            src="https://i.imgur.com/rK6cgqb.png" 
+                            alt="c" 
+                            data-tip="C"
+                            onMouseEnter={() => showTooltip(true)}
+                            onMouseLeave={() => {
+                                  showTooltip(false);
+                                  setTimeout(() => showTooltip(true), 0);
+                                }}
+                            />
+                        </>
+                        <>
+                        {tooltip && <ReactTooltip effect="solid" />}
+                            <img 
+                            className="skill_icon" 
+                            src="https://i.imgur.com/J9vqWty.png" 
+                            alt="c++" 
+                            data-tip="C++"
+                            onMouseEnter={() => showTooltip(true)}
+                            onMouseLeave={() => {
+                                  showTooltip(false);
+                                  setTimeout(() => showTooltip(true), 0);
+                                }}
+                            />
+                        </>
                     </div>
                 <h3>Libraries and Frameworks</h3>
                     <div className="skill_icons">
-                        <img className="skill_icon" src="https://i.imgur.com/ePTfjzt.png" alt="react"/>
-                        <img className="skill_icon" src="https://i.imgur.com/Ke6koPJ.png" alt="node.js"/>
-                        <img className="skill_icon" src="https://i.imgur.com/BX4UdjY.png" alt="express.js"/>
-                        <img className="skill_icon" src="https://i.imgur.com/5xk8xhR.png" alt="mongodb"/>
-                        <img className="skill_icon" src="https://i.imgur.com/gQMAhsd.png" alt="jquery"/>
-                        <img className="skill_icon contrast" src="https://pandas.pydata.org/static/img/pandas_secondary.svg" alt="pandas"/>
-                        <img className="skill_icon" src="https://i.imgur.com/JyDaA3s.png" alt="openpyxl"/>
-                        <img className="skill_icon" src="https://i.imgur.com/RWlPK3i.png" alt="matplotlib"/>
-                        <img className="skill_icon contrast" src="https://user-images.githubusercontent.com/67586773/105040771-43887300-5a88-11eb-9f01-bee100b9ef22.png" alt="numpy"/>
-                        <img className="skill_icon" src="https://scipy.org/images/logo.svg" alt="scipy"/>
+                        <>
+                        {tooltip && <ReactTooltip effect="solid" />}
+                            <img 
+                            className="skill_icon" 
+                            src="https://i.imgur.com/ePTfjzt.png"
+                            alt="react" 
+                            data-tip="React"
+                            onMouseEnter={() => showTooltip(true)}
+                            onMouseLeave={() => {
+                                  showTooltip(false);
+                                  setTimeout(() => showTooltip(true), 0);
+                                }}
+                            />
+                        </>
+                        <>
+                        {tooltip && <ReactTooltip effect="solid" />}
+                            <img 
+                            className="skill_icon" 
+                            src="https://i.imgur.com/Ke6koPJ.png"
+                            alt="node.js" 
+                            data-tip="Node"
+                            onMouseEnter={() => showTooltip(true)}
+                            onMouseLeave={() => {
+                                  showTooltip(false);
+                                  setTimeout(() => showTooltip(true), 0);
+                                }}
+                            />
+                        </>
+                        <>
+                        {tooltip && <ReactTooltip effect="solid" />}
+                            <img 
+                            className="skill_icon" 
+                            src="https://i.imgur.com/BX4UdjY.png"
+                            alt="express" 
+                            data-tip="Express"
+                            onMouseEnter={() => showTooltip(true)}
+                            onMouseLeave={() => {
+                                  showTooltip(false);
+                                  setTimeout(() => showTooltip(true), 0);
+                                }}
+                            />
+                        </>
+                        <>
+                        {tooltip && <ReactTooltip effect="solid" />}
+                            <img 
+                            className="skill_icon" 
+                            src="https://i.imgur.com/5xk8xhR.png"
+                            alt="mongodb" 
+                            data-tip="MongoDB"
+                            onMouseEnter={() => showTooltip(true)}
+                            onMouseLeave={() => {
+                                  showTooltip(false);
+                                  setTimeout(() => showTooltip(true), 0);
+                                }}
+                            />
+                        </>
+                        <>
+                        {tooltip && <ReactTooltip effect="solid" />}
+                            <img 
+                            className="skill_icon" 
+                            src="https://i.imgur.com/gQMAhsd.png"
+                            alt="jquery" 
+                            data-tip="jQuery"
+                            onMouseEnter={() => showTooltip(true)}
+                            onMouseLeave={() => {
+                                  showTooltip(false);
+                                  setTimeout(() => showTooltip(true), 0);
+                                }}
+                            />
+                        </>
+                        <>
+                        {tooltip && <ReactTooltip effect="solid" />}
+                            <img 
+                            className="skill_icon contrast" 
+                            src="https://pandas.pydata.org/static/img/pandas_secondary.svg"
+                            alt="pandas" 
+                            data-tip="pandas"
+                            onMouseEnter={() => showTooltip(true)}
+                            onMouseLeave={() => {
+                                  showTooltip(false);
+                                  setTimeout(() => showTooltip(true), 0);
+                                }}
+                            />
+                        </>
+                        <>
+                        {tooltip && <ReactTooltip effect="solid" />}
+                            <img 
+                            className="skill_icon" 
+                            src="https://i.imgur.com/JyDaA3s.png" 
+                            alt="openpyxl" 
+                            data-tip="Openpyxl"
+                            onMouseEnter={() => showTooltip(true)}
+                            onMouseLeave={() => {
+                                  showTooltip(false);
+                                  setTimeout(() => showTooltip(true), 0);
+                                }}
+                            />
+                        </>
+                        <>
+                        {tooltip && <ReactTooltip effect="solid" />}
+                            <img 
+                            className="skill_icon" 
+                            src="https://i.imgur.com/RWlPK3i.png"
+                            alt="matplotlib" 
+                            data-tip="Matplotlib"
+                            onMouseEnter={() => showTooltip(true)}
+                            onMouseLeave={() => {
+                                  showTooltip(false);
+                                  setTimeout(() => showTooltip(true), 0);
+                                }}
+                            />
+                        </>
+                        <>
+                        {tooltip && <ReactTooltip effect="solid" />}
+                            <img 
+                            className="skill_icon contrast" 
+                            src="https://user-images.githubusercontent.com/67586773/105040771-43887300-5a88-11eb-9f01-bee100b9ef22.png"
+                            alt="numpy" 
+                            data-tip="NumPy"
+                            onMouseEnter={() => showTooltip(true)}
+                            onMouseLeave={() => {
+                                  showTooltip(false);
+                                  setTimeout(() => showTooltip(true), 0);
+                                }}
+                            />
+                        </>
+                        <>
+                        {tooltip && <ReactTooltip effect="solid" />}
+                            <img 
+                            className="skill_icon" 
+                            src="https://scipy.org/images/logo.svg"
+                            alt="scipy" 
+                            data-tip="SciPy"
+                            onMouseEnter={() => showTooltip(true)}
+                            onMouseLeave={() => {
+                                  showTooltip(false);
+                                  setTimeout(() => showTooltip(true), 0);
+                                }}
+                            />
+                        </>
                     </div>
                 <h3>Software</h3>
                     <div className="skill_icons">
-                        <img className="skill_icon" src="https://i.imgur.com/04j7fLZ.png" alt="scipy"/>
+                    <>
+                        {tooltip && <ReactTooltip effect="solid" />}
+                            <img 
+                            className="skill_icon" 
+                            src="https://i.imgur.com/04j7fLZ.png"
+                            alt="command line"
+                            data-tip="Command Line"
+                            onMouseEnter={() => showTooltip(true)}
+                            onMouseLeave={() => {
+                                  showTooltip(false);
+                                  setTimeout(() => showTooltip(true), 0);
+                                }}
+                            />
+                        </>
                     </div>
 
             </div>
