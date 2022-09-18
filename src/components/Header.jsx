2639423386d10d265ac795c081aeee9e30cb4c77
matt pero <mattpero22@gmail.com>
2022-09-18 16:32:24 -0400

    templated out project cards and placement of projects. Setup for 4 featured and then in future will have 'see all'

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,10 @@ import { HashLink } from 'react-router-hash-link'
 const Header = () => {
     return (
             <ul className="Header">
-                <HashLink className='header_link hover_animate' smooth to={'/#'}><img src="https://i.imgur.com/jYrWq1Z.png" alt="home"></img><p>Home</p></HashLink>
+                <HashLink className='header_link hover_animate' smooth to={'/#'}>
+                    <img src="https://i.imgur.com/jYrWq1Z.png" alt="home"></img>
+                    <p className='hover_reveal'>Home</p>
+                </HashLink>
                 <HashLink className='header_link hover_animate' smooth to={'/#about'}><img src="https://i.imgur.com/mPcpvxE.png" alt="me"></img><p>About</p></HashLink>
                 <HashLink className='header_link hover_animate' smooth to={'/#skills'}><img src="https://i.imgur.com/glFzRCv.png" alt="skills"></img><p>Skills</p></HashLink>
                 <HashLink className='header_link hover_animate' smooth to={'/#projects'}><img src="https://i.imgur.com/aHKbJtv.png" alt="projects"></img><p>Projects</p></HashLink>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,47 @@
+import { useState } from 'react';
+
 const Projects = () => {
     return (
         <div className="Projects" id="projects">
             <div className="projects_content">
                 <h2>Projects</h2>
-                <h3 className="project_card">Project 1 Title</h3>
+                <div className="project_card">
+                    <h3 className="project_title">Project 1 Title</h3>
+                    <img className="project_thumbnail" src='https://i.imgur.com/WP1B1y5.png'></img>
+                    <div className="project_buttons">
+                        <a className='project_button hover_animate header_link' href="">About</a>
+                        <a className='project_button hover_animate header_link'>Github</a>
+                        <a className='project_button hover_animate header_link'>Deployed</a>
+                    </div>
+                </div>
+                <div className="project_card alt_align">
+                    <h3 className="project_title header_link">Pokemon Auto Battler</h3>
+                    <img className="project_thumbnail" src='https://i.imgur.com/WP1B1y5.png'></img>
+                    <div className="project_buttons">
+                        <a className='project_button hover_animate header_link' href="" target="_blank">About</a>
+                        <a className='project_button hover_animate header_link'>Github</a>
+                        <a className='project_button hover_animate header_link'>Deployed</a>
+                    </div>
+                </div>
+                <div className="project_card">
+                    <h3 className="project_title">Project 1 Title</h3>
+                    <img className="project_thumbnail" src='https://i.imgur.com/WP1B1y5.png'></img>
+                    <div className="project_buttons">
+                        <a className='project_button hover_animate header_link' href="">About</a>
+                        <a className='project_button hover_animate header_link'>Github</a>
+                        <a className='project_button hover_animate header_link'>Deployed</a>
+                    </div>
+                </div>
+                <div className="project_card alt_align">
+                    <h3 className="project_title">Project 1 Title</h3>
+                    <img className="project_thumbnail" src='https://i.imgur.com/WP1B1y5.png'></img>
+                    <div className="project_buttons">
+                        <a className='project_button hover_animate header_link' href="">About</a>
+                        <a className='project_button hover_animate header_link'>Github</a>
+                        <a className='project_button hover_animate header_link'>Deployed</a>
+                    </div>
+                </div>
+                
             </div>
         </div>
     )
