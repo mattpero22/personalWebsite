6730f78599eefbe94fc541e6afd6bb7ae963ad78
matt pero <mattpero22@gmail.com>
2022-09-20 09:30:55 -0400

    commit work from last night and added state for social icons in the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,32 @@
+import { useState } from "react";
+
 const Footer = () => {
+    const gitWhite = "https://i.imgur.com/rL2jKXE.png"
+    const gitPurple = "https://imgur.com/7IamkrW.png"
+
+    const [ gitLink, setGitLink ] = useState(gitWhite)
+
+    const handleGitLink = () =>  setGitLink(gitPurple)
+
+    const handleGitLinkOff = () => setGitLink(gitWhite)
+
     return (
-        <ul className="Footer">
-            <li>© Matthew Pero 2022</li>
-        </ul>
+        <>
+            <ul className="Socials">
+                <li>
+                    <img 
+                        onMouseOver={handleGitLink} 
+                        onMouseOut={handleGitLinkOff} 
+                        src={gitLink}
+                        className='social_icon'
+                    >
+                    </img>
+                </li>
+            </ul>
+            <ul className="Footer">
+                <li>© Matthew Pero 2022</li>
+            </ul>
+        </>
     );
 }
 
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,10 +6,10 @@ const Projects = () => {
             <div className="projects_content">
                 <h2>Projects</h2>
                 <div className="project_card">
-                    <h3 className="project_title">Project 1 Title</h3>
-                    <img className="project_thumbnail" src='https://i.imgur.com/WP1B1y5.png'></img>
+                    <h3 className="project_title header_link">P3 Password Manager</h3>
+                    <img className="project_thumbnail" src='https://i.imgur.com/pLnrklh.png'></img>
                     <div className="project_buttons">
-                        <a className='project_button hover_animate header_link' href="">About</a>
+                        <a className='project_button hover_animate header_link'>About</a>
                         <a className='project_button hover_animate header_link'>Github</a>
                         <a className='project_button hover_animate header_link'>Deployed</a>
                     </div>
@@ -18,25 +18,25 @@ const Projects = () => {
                     <h3 className="project_title header_link">Pokemon Auto Battler</h3>
                     <img className="project_thumbnail" src='https://i.imgur.com/WP1B1y5.png'></img>
                     <div className="project_buttons">
-                        <a className='project_button hover_animate header_link' href="" target="_blank">About</a>
+                        <a className='project_button hover_animate header_link' target="_blank">About</a>
                         <a className='project_button hover_animate header_link'>Github</a>
                         <a className='project_button hover_animate header_link'>Deployed</a>
                     </div>
                 </div>
                 <div className="project_card">
-                    <h3 className="project_title">Project 1 Title</h3>
-                    <img className="project_thumbnail" src='https://i.imgur.com/WP1B1y5.png'></img>
+                    <h3 className="project_title header_link">General Store</h3>
+                    <img className="project_thumbnail" src='https://i.imgur.com/sEEA3ab.png'></img>
                     <div className="project_buttons">
-                        <a className='project_button hover_animate header_link' href="">About</a>
+                        <a className='project_button hover_animate header_link'>About</a>
                         <a className='project_button hover_animate header_link'>Github</a>
                         <a className='project_button hover_animate header_link'>Deployed</a>
                     </div>
                 </div>
                 <div className="project_card alt_align">
-                    <h3 className="project_title">Project 1 Title</h3>
-                    <img className="project_thumbnail" src='https://i.imgur.com/WP1B1y5.png'></img>
+                    <h3 className="project_title header_link">Pebble Prints</h3>
+                    <img className="project_thumbnail" src='https://i.imgur.com/pPepyyz.png'></img>
                     <div className="project_buttons">
-                        <a className='project_button hover_animate header_link' href="">About</a>
+                        <a className='project_button hover_animate header_link'>About</a>
                         <a className='project_button hover_animate header_link'>Github</a>
                         <a className='project_button hover_animate header_link'>Deployed</a>
                     </div>
