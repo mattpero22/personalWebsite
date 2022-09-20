ba95d4fada0bdc604384439fe19bbca795683f14
matt pero <mattpero22@gmail.com>
2022-09-20 15:08:49 -0400

    added linked in social icon and changed their position and style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,40 @@
 import { useState } from "react";
 
 const Footer = () => {
-    const gitWhite = "https://i.imgur.com/rL2jKXE.png"
-    const gitPurple = "https://imgur.com/7IamkrW.png"
+    const gitWhite = "https://i.imgur.com/Vjr9ESf.png"
+    const gitPurple = "https://i.imgur.com/m6JwhOE.png"
+    const inWhite = "https://i.imgur.com/1teiCPo.png"
+    const inPurple = "https://i.imgur.com/MN4xqQM.png"
 
     const [ gitLink, setGitLink ] = useState(gitWhite)
-
     const handleGitLink = () =>  setGitLink(gitPurple)
-
     const handleGitLinkOff = () => setGitLink(gitWhite)
+
+    const [ inLink, setInLink ] = useState(inWhite)
+    const handleInLink = () =>  setInLink(inPurple)
+    const handleInLinkOff = () => setInLink(inWhite)
 
     return (
         <>
             <ul className="Socials">
-                <li>
+                <a href="https://github.com/mattpero22">
                     <img 
                         onMouseOver={handleGitLink} 
                         onMouseOut={handleGitLinkOff} 
                         src={gitLink}
                         className='social_icon'
-                    >
+                        alt='github'>
                     </img>
-                </li>
+                </a>
+                <a href="https://www.linkedin.com/in/matthew-pero22/">
+                    <img 
+                        onMouseOver={handleInLink} 
+                        onMouseOut={handleInLinkOff} 
+                        src={inLink}
+                        className='social_icon'
+                        alt='github'>
+                    </img>
+                </a>
             </ul>
             <ul className="Footer">
                 <li>© Matthew Pero 2022</li>
